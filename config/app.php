11525a4a3fae4a78798ad332a3f6fac8d764dc8a
Raphael Thibierge <raph.t.id@gmail.com>
2017-09-03 18:03:40 -0400

    change app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'ProductivityApp'),
+    'name' => env('APP_NAME', 'Sir Edgard'),
 
     /*
     |--------------------------------------------------------------------------
