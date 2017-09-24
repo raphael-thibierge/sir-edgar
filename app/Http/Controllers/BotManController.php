51c3d6541af9883d22097b00cba17e3e14f2843e
Raphael Thibierge <raph.t.id@gmail.com>
2017-09-24 17:59:08 -0400

    adit welcome message

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -6,6 +6,10 @@ use App\Conversations\MorningConversation;
 use App\Goal;
 use App\User;
 
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\Drivers\Facebook\Extensions\Element;
+use BotMan\Drivers\Facebook\Extensions\ElementButton;
+use BotMan\Drivers\Facebook\Extensions\GenericTemplate;
 use Illuminate\Http\Request;
 use Mockery\Exception;
 use Mpociot\BotMan\BotMan;
@@ -18,25 +22,16 @@ class BotManController extends Controller
     public function handle()
     {
 
-
         $botman = app('botman');
         $botman->verifyServices(env('FACEBOOK_VERIFICATION'));
 
-
         // give the bot something to listen for.
-        $botman->hears('hello', function (BotMan $bot) {
-            $bot->reply('Hello yourself.');
-        });
-
-        // give the bot something to listen for.
-        $botman->hears('hi', function (BotMan $bot) {
-            $bot->reply('Yooo');
+        $botman->hears('Hi', function (BotMan $bot) {
+            $bot->reply('Hello ! A lot of new features are coming ! See you soon ;)');
         });
 
         // start listening
         $botman->listen();
-
-
 
     }
 
