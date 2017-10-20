0598114d97bcde80186d7d16540c3dea291ed116
Raphael Thibierge <raph.t.id@gmail.com>
2017-10-19 21:01:12 -0400

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+        <div class="offset-sm-2 col-sm-8">
+            <div class="card">
+                <h4 class="card-header">Login</h4>
+                <div class="card-body">
+                    
+                    <form role="form" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         @if(isset($_REQUEST['redirect_uri']))
@@ -23,10 +24,17 @@
                         <input type="hidden" name="account_linking_token" value="{{ $_REQUEST['account_linking_token'] }}">
                         @endif
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group row">
+                            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+                            </div>
+                        </div>
 
-                            <div class="col-md-6">
+                        <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }} row ">
+                            <label for="email" class="col-sm-2 form-control-label">Email</label>
+
+                            <div class="col-sm-10">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -37,10 +45,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group {{ $errors->has('password') ? ' has-error' : '' }} row">
+                            <label for="password" class="col-sm-2 form-control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-sm-10">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -51,18 +59,23 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                        <div class="form-group row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input type="checkbox" class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        Remember Me
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
+
+                        <div class="form-group row">
+
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-10">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
