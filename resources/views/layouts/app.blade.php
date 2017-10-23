728fdb1fae900d829bfd7922156b8c85e4f4c8b1
Raphael Thibierge <raph.t.id@gmail.com>
2017-10-23 18:04:36 -0400

    AdSense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,14 @@
         ga('send', 'pageview');
 
     </script>
+    <!-- Google AdSense -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-6757486619789376",
+            enable_page_level_ads: true
+        });
+    </script>
     @endif
 
 </head>
