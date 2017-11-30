b1cb0935d2c124c1f2f6ea5d5f9dc099fde2716a
Raphael Thibierge <raph.t.id@gmail.com>
2017-11-30 11:12:58 -0500

    fix coinbase job

--- app/Jobs/UpdateCoinbasePriceJob.php
+++ app/Jobs/UpdateCoinbasePriceJob.php
@@ -37,20 +37,20 @@ class UpdateCoinbasePriceJob implements ShouldQueue
 
         $btc = [
             'sell' => $client->getBuyPrice('BTC-EUR')->getAmount(),
-            'spot' => $btc = $client->getSpotPrice('BTC-EUR')->getAmount(),
-            'buy' => $btc = $client->getBuyPrice('BTC-EUR')->getAmount(),
+            'spot' => $client->getSpotPrice('BTC-EUR')->getAmount(),
+            'buy' => $client->getBuyPrice('BTC-EUR')->getAmount(),
         ];
 
         $eth = [
             'sell' => $client->getBuyPrice('ETH-EUR')->getAmount(),
-            'spot' => $btc = $client->getSpotPrice('ETH-EUR')->getAmount(),
-            'buy' => $btc = $client->getBuyPrice('ETH-EUR')->getAmount(),
+            'spot' => $client->getSpotPrice('ETH-EUR')->getAmount(),
+            'buy' => $client->getBuyPrice('ETH-EUR')->getAmount(),
         ];
 
         $ltc = [
             'sell' => $client->getBuyPrice('LTC-EUR')->getAmount(),
-            'spot' => $btc = $client->getSpotPrice('LTC-EUR')->getAmount(),
-            'buy' => $btc = $client->getBuyPrice('LTC-EUR')->getAmount(),
+            'spot' => $client->getSpotPrice('LTC-EUR')->getAmount(),
+            'buy' => $client->getBuyPrice('LTC-EUR')->getAmount(),
         ];
 
         event(new UpdateCoinbaseEvent([
