22f86ae2235eb3fabb8a9564a5691fe8d7a10f1c
Raphael Thibierge <raph.t.id@gmail.com>
2018-02-28 16:16:11 -0500

    add total daily expense in table

--- resources/assets/js/components/expense/ExpenseTable.jsx
+++ resources/assets/js/components/expense/ExpenseTable.jsx
@@ -11,48 +11,73 @@ export default class ExpenseTable extends React.Component {
         }
     }
 
+    dateLineRender(date, total){
+
+        const style = {
+            borderTop: '50px solid transparent',
+            backgroundClip: 'padding-box'
+        };
+
+        return (
+            <tr key={date.toTimeString()} className="active" style={style}>
+                <td >
+                    <h4>
+                        {Tools.getDaysOfWeek()[date.getDay()]}
+                        {',  the '}
+                        {Tools.getDaySuffix(date)}
+                    </h4></td>
+                <td><h4>{Math.round(total*100)/100}</h4></td>
+                <td><h4>CAD</h4></td>
+                <td></td>
+                <td colSpan={2}>
+                    <h4>
+                        {Tools.getMonthsOfYear()[date.getMonth()]}
+                        {' '}
+                        {date.getFullYear()}
+                    </h4>
+                </td>
+            </tr>
+        );
+    }
+
     render(){
 
         let expensesRended = [];
 
 
         let previous = null;
-        this.props.expenses.reverse().forEach((expense) => {
+        let total  = 0;
 
+        this.props.expenses.forEach((expense) => {
             let date = Tools.dateFormater(expense.date);
             console.log(date);
-            console.log(expense.title);
 
-            if (previous === null || date.toLocaleDateString() !== previous.toLocaleDateString()){
-
-                expensesRended.push(
-                    <tr key={date.toTimeString()} className="active">
-                        <td colSpan={3}>
-                            <h4>
-                                {Tools.getDaysOfWeek()[date.getDay()]}
-                                {',  the '}
-                                {Tools.getDaySuffix(date)}
-                            </h4></td>
-                        <td colSpan={2}></td>
-                        <td>
-                            <h4>
-                                {Tools.getMonthsOfYear()[date.getMonth()]}
-                                {' '}
-                                {date.getFullYear()}
-                            </h4>
-                        </td>
-                    </tr>
-                );
-
+            if (previous === null){
                 previous = date;
             }
+
+            if (expense.type === 'expense'){
+                total -= expense.price;
+            } else {
+                total += expense.price;
+            }
+
+            if (date.toLocaleDateString() !== previous.toLocaleDateString()){
+
+                expensesRended.push(this.dateLineRender(previous, total));
+
+                // reset daily total expense
+                previous = date;
+                total = 0;
+            }
+
 
             expensesRended.push(
                 <tr key={expense._id} className={expense.type === 'entrance' ? 'success' : null}>
                     <td>{expense.title}</td>
                     <td>{expense.price}</td>
                     <td>{expense.currency}</td>
-                    <td>{Tools.dateFormatWithOffset(expense.date).toLocaleTimeString()}</td>
+                    <td>{Tools.dateFormater(expense.date).toLocaleTimeString()}</td>
                     <td>
                         <CreateFinancialTransactionModal
                             expense={expense}
@@ -62,8 +87,13 @@ export default class ExpenseTable extends React.Component {
                     <td>{Array.isArray(expense.tags) ? expense.tags.join(', ') : expense.tags}</td>
                 </tr>
             );
+
+
         });
 
+        if (this.props.expenses.length > 0){
+            expensesRended.push(this.dateLineRender(previous, total));
+        }
 
         return (
             <div className="row">
@@ -71,7 +101,7 @@ export default class ExpenseTable extends React.Component {
                     <div className="table-responsive">
                         <table className="table">
                             <tbody>
-                                {expensesRended}
+                                {expensesRended.reverse()}
                                 <tr><td colSpan={6}></td></tr>
                                 <tr>
                                     <td>{this.props.expenses.totalTransactions()}</td>
