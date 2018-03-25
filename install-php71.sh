60d8adb374ad8c6e18d7d3857208d58813d1f068
Raphael Thibierge <raph.t.id@gmail.com>
2018-03-25 17:29:05 -0400

    fix install script

--- install-php71.sh
+++ install-php71.sh
@@ -12,4 +12,4 @@ sudo apt-get update
 sudo apt-get install -y php7.1-fpm php7.1-mysql php7.1-curl php7.1-gd php7.1-json php7.1-mcrypt php7.1-opcache php7.1-xml php7.1-mbstring php7.1-dev php7.1-intl php7.1-zip php7.1-gd php7.1-mongodb unzip mongodb
 
 # install composer
-sudp apt-get install composer
+sudo apt-get install composer
