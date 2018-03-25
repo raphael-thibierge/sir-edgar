e6b84e9a7a8da6270697e2261bbeda4feb88bf29
Raphael Thibierge <raph.t.id@gmail.com>
2018-03-25 17:27:28 -0400

    install php packages script

---
+++ install-php71.sh
@@ -0,0 +1,15 @@
+sudo apt-get install software-properties-common
+
+# add php7 repository
+sudo add-apt-repository ppa:ondrej/php
+
+#if probleme with GPG key not founf :
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys <KEY>
+
+sudo apt-get update
+
+#install php7 packages
+sudo apt-get install -y php7.1-fpm php7.1-mysql php7.1-curl php7.1-gd php7.1-json php7.1-mcrypt php7.1-opcache php7.1-xml php7.1-mbstring php7.1-dev php7.1-intl php7.1-zip php7.1-gd php7.1-mongodb unzip mongodb
+
+# install composer
+sudp apt-get install composer
